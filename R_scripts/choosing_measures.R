0e9d3d1d65101acbe3e312e30b0890c2cfa8f469
moramaldonado <mormaldo@gmail.com>
2017-01-24 17:15:19 +0100

    Calibration analyses

--- R_scripts/choosing_measures.R
+++ R_scripts/choosing_measures.R
@@ -1,24 +1,50 @@
 
 ## WHICH MEASURE IS A BETTER PREDICTOR FOR THE CALIBRATION ##
-calibration_data.summary <- ddply(calibration_data, c("Polarity"),
-                                  function(calibration_data.summary)c(MaxLogRatio.mean=mean(calibration_data.summary$MaxLogRatio, na.rm=T), MaxLogRatio.se=se(calibration_data.summary$MaxLogRatio, na.rm=T), MaxDifference.mean=mean(calibration_data.summary$MaxDifference, na.rm=T), MaxDifference.se=se(calibration_data.summary$MaxDifference, na.rm=T)))
-
-ggplot(calibration_data, aes(x=Polarity, y=) )
 
 #1. MaxLogRatio
 #2. MaxDifference
 #3. MaxDeviation
 
-calibration_ratio.lmer <- lmer(MaxLogRatio~Polarity+(1|Subject), data = calibration_data)
-calibration_data.lmer_sum <- summary(calibration_ratio.lmer)
+##SIMULATION FAKE DATA: Create fake data and run model 
+n = 50
+# Make new subjects
+new_subjects = calibration_data %>%
+# Select 2 unique subjects
+select(Subject) %>%
+distinct() %>%
+# Sample subjects n times with replacement
+sample_n(n, replace = T) %>%
+# Order by original subject ID
+arrange(Subject) %>%
+# Assign subject IDs 1 to n for sampled subjects
+mutate(randomID = paste("randomID", 1:n, sep = "_"))%>%
+# Add some variance per subject
+mutate(MaxLogRatio_sd = rnorm(n, 0, sd(calibration_data$MaxLogRatio)))%>%
+mutate(MaxDifference_sd = rnorm(n, 0, sd(calibration_data$MaxDifference)))%>%  
+mutate(MaxDeviation_sd = rnorm(n, 0, sd(calibration_data$MaxDeviation)))
 
-calibration_ratio.lmer <- lmer(MaxDeviation~Polarity+(1|Subject), data = calibration_data)
-calibration_data.lmer_sum <- summary(calibration_ratio.lmer)
+# Make data for new subjects
+calibration_data_new_subjects = calibration_data %>%
+# Combine original data with list of new subjects to get
+inner_join(new_subjects) %>%
+# Make a new column for dependent variable with added variance
+mutate(MaxLogRatio_NEW = MaxLogRatio + MaxLogRatio_sd) %>%
+mutate(MaxDifference_NEW = MaxDifference + MaxDifference_sd) %>%
+mutate(MaxDeviation_NEW = MaxLogRatio + MaxDeviation_sd) 
 
 
+# Run model and save result
+calibration_data_new_subjects$Polarity_num <- as.numeric(factor(calibration_data_new_subjects$Polarity))-1
+calibration.lmer <- lmer(Polarity_num ~ MaxLogRatio_NEW  +
+                            (1|randomID), data = calibration_data_new_subjects)
+calibration.lmer_sum = summary(calibration.lmer)
+calibration.lmer_anova = anova(calibration.lmer)
 
-##Bootstrapping the number of calibration
-n = 100
+calibration_data.summary <- ddply(calibration_data_new_subjects, c("Polarity"),
+                                  function(calibration_data.summary)c(MaxLogRatio.mean=mean(calibration_data.summary$MaxLogRatio_NEW, na.rm=T), MaxLogRatio.se=se(calibration_data.summary$MaxLogRatio_NEW, na.rm=T), MaxDifference.mean=mean(calibration_data.summary$MaxDifference_NEW, na.rm=T), MaxDifference.se=se(calibration_data.summary$MaxDifference_NEW, na.rm=T), MaxDeviation.mean=mean(calibration_data.summary$MaxDeviation_NEW, na.rm=T), MaxDeviation.se=se(calibration_data.summary$MaxDeviation_NEW, na.rm=T)))
 
-## Linear regression
-calibration.lmer <- lmer()
+#plotting
+p1 <- ggplot(calibration_data.summary, aes(x=Polarity, y=MaxLogRatio.mean, color=Polarity)) + geom_point() + geom_errorbar(aes(ymin=MaxLogRatio.mean-MaxLogRatio.se,   ymax=MaxLogRatio.mean+MaxLogRatio.se), width=.1) 
+p2 <- ggplot(calibration_data.summary, aes(x=Polarity, y=MaxDifference.mean, color=Polarity)) + geom_point()  + geom_point() + geom_errorbar(aes(ymin=MaxDifference.mean-MaxDifference.se,   ymax=MaxDifference.mean+MaxDifference.se), width=.1) 
+p3 <- ggplot(calibration_data.summary, aes(x=Polarity, y=MaxDeviation.mean , color=Polarity)) + geom_point() + geom_point() + geom_errorbar(aes(ymin=MaxDeviation.mean-MaxDeviation.se,   ymax=MaxDeviation.mean+MaxDeviation.se), width=.1) 
+multiplot(p1,p2,p3)
--- R_scripts/compile.R
+++ R_scripts/compile.R
@@ -1,5 +1,9 @@
 
-
+##1. CHARGE PACKAGES
 source("R_scripts/packages.R")
 
+## 2. EXTRACTING_DATA
 source("R_scripts/extracting_data.R")
+
+## 3. CALIBRATION / Choosing measures
+source("R_scripts/choosing_measures.R")
