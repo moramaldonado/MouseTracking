ea983220e6753cf880506eabc134392f5c6b5c8e
moramaldonado <mormaldo@gmail.com>
2017-08-28 18:29:51 +0200

    Random classifier

--- R_scripts/calibration/random_classifier.R
+++ R_scripts/calibration/random_classifier.R
@@ -16,49 +16,50 @@ calibration_data <- subset(calibration_data, Polarity!='uncertain')
 iterations = 100
 random_classifier.df <- data.frame(matrix(ncol=iterations, nrow=10))
 
+# Create bins for crossvalidation
+calibration_data$id <- 1:nrow(calibration_data)
+deviated <- calibration_data %>% 
+  filter(Polarity=='deviated')%>%
+  dplyr::select(id)
+deviated$id <- sample(deviated$id) 
+
+straight <- calibration_data %>% 
+  filter(Polarity=='straight')%>%
+  dplyr::select(id)
+straight$id <- sample(straight$id) 
+
+bins<- rep(1:10, nrow(straight) / 10)
+try <- split(straight, bins)
+
+bin1 <- try$`1`
+bin2 <- try$`2`
+bin3 <- try$`3`
+bin4 <- try$`4`
+bin5 <- try$`5`
+bin6 <- try$`6`
+bin7 <- try$`7`
+bin8 <- try$`8`
+bin9 <- try$`9`
+bin10 <- try$`10`
+
+bins  <- rep(1:10, nrow(deviated) / 10)
+try <- split(deviated, bins)
+
+bin1 <- rbind(bin1, try$`1`)
+bin2 <- rbind(bin2, try$`2`)
+bin3 <- rbind(bin3, try$`3`)
+bin4 <- rbind(bin4, try$`4`)
+bin5 <- rbind(bin5, try$`5`)
+bin6 <- rbind(bin6, try$`6`)
+bin7 <- rbind(bin7, try$`7`)
+bin8 <- rbind(bin8, try$`8`)
+bin9 <- rbind(bin9, try$`9`)
+bin10 <- rbind(bin10, try$`10`)
+rm(try)
+bins <- list(bin1, bin2, bin3, bin4, bin5, bin6, bin7, bin8, bin9, bin10) 
+
 for (i in 1:iterations){
-  # Create bins for crossvalidation
-    calibration_data$id <- 1:nrow(calibration_data)
-    deviated <- calibration_data %>% 
-      filter(Polarity=='deviated')%>%
-      dplyr::select(id)
-    deviated$id <- sample(deviated$id) 
-    
-    straight <- calibration_data %>% 
-      filter(Polarity=='straight')%>%
-      dplyr::select(id)
-    straight$id <- sample(straight$id) 
-    
-    bins<- rep(1:10, nrow(straight) / 10)
-    try <- split(straight, bins)
-    
-    bin1 <- try$`1`
-    bin2 <- try$`2`
-    bin3 <- try$`3`
-    bin4 <- try$`4`
-    bin5 <- try$`5`
-    bin6 <- try$`6`
-    bin7 <- try$`7`
-    bin8 <- try$`8`
-    bin9 <- try$`9`
-    bin10 <- try$`10`
-    
-    bins  <- rep(1:10, nrow(deviated) / 10)
-    try <- split(deviated, bins)
-    
-    bin1 <- rbind(bin1, try$`1`)
-    bin2 <- rbind(bin2, try$`2`)
-    bin3 <- rbind(bin3, try$`3`)
-    bin4 <- rbind(bin4, try$`4`)
-    bin5 <- rbind(bin5, try$`5`)
-    bin6 <- rbind(bin6, try$`6`)
-    bin7 <- rbind(bin7, try$`7`)
-    bin8 <- rbind(bin8, try$`8`)
-    bin9 <- rbind(bin9, try$`9`)
-    bin10 <- rbind(bin10, try$`10`)
-    rm(try)
-    bins <- list(bin1, bin2, bin3, bin4, bin5, bin6, bin7, bin8, bin9, bin10) 
-    
+     
     # ROC and AUC for each bin
     for (b in 1: length(bins)) {
       calibrationTrain <- subset(calibration_data, !(id %in% bins[[b]]$id))
