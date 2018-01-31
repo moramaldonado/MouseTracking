f38e9192be33ca3f5f84b39663ef4edf95bd46bb
moramaldonado <mormaldo@gmail.com>
2018-01-31 10:19:16 +0100

    paper
    old and new tex files for paper + R script to generate paper data +
    figures

---
+++ paper/R/compile.R
@@ -0,0 +1,13 @@
+## Compling file for paper
+
+## CLEAN ENVIRONMENT
+rm(list = ls())
+
+## CHARGE PACKAGES AND FUNCTIONS
+source("paper/R/packages-functions.R")
+
+## VALIDATION EXPERIMENT ####
+source("paper/R/validation.R")
+
+## NEGATION EXPERIMENT ####
+source("paper/R/negation.R")
